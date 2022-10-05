0dcf847e190bb3b0e108e8e647535e69e40875cf
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-10-05 17:01:14 +0700

    draft: All meetups components splitting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 const App = () => {
   return (
-    <div>ajos</div>
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetupItem = () => {
+  return (
+    <li>
+        
+    </li>
+  )
+}
+
+export default MeetupItem
