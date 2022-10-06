1babe68a085215b65414dcf7f01c44127a7a0d59
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-10-06 10:05:16 +0700

    added new meetup form to new meetup page

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 const NewMeetup = () => {
   return (
-    <div>NewMeetup</div>
+    <div>
+      <h1>New Meetup</h1>
+      <NewMeetupForm />
+    </div>
   )
 }
 
