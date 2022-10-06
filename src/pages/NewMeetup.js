27e3e4b45eaad75ed05bf84722c09d5c03da26bd
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-10-06 11:21:44 +0700

    navigate pragmatically, fetching data from firebase, and list state

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,22 +1,30 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm"
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
+  const history = useHistory();
+
   const handleAddMeetup = (meetupData) => {
-    fetch('https://react-refresher-2eb39-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json', {
-      method: 'POST',
-      body: JSON.stringify(meetupData),
-      headers: {
-        'Content-Type': 'application/json'
+    fetch(
+      "https://react-refresher-2eb39-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    })
-  }
+    ).then(() => {
+      history.replace('/');
+    });
+  };
 
   return (
     <div>
       <h1>New Meetup</h1>
       <NewMeetupForm onAddMeetup={handleAddMeetup} />
     </div>
-  )
-}
+  );
+};
 
-export default NewMeetup
+export default NewMeetup;
